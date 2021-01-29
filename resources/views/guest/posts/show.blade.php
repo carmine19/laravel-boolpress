82c7561ab8e2f397ac8e6d78f120f9a023d2abe7
carmine <carminepepiciello@gmail.com>
2021-01-29 16:23:35 +0100

    sistemazione layouts show front office

--- resources/views/guest/posts/show.blade.php
+++ resources/views/guest/posts/show.blade.php
@@ -1,32 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1>{{ $post->title }}</h1>
-                <div>
-                    {{ $post->content }}
-                </div>
-                <div class="">
-                    <p>Categoria:
-                    @if ($post->category)
-                        {{ $post->category->name }}
-                    @else
-                        -
-                    @endif
-                </p>
-                </div>
-                <div class="">
-                    <p>Tags:
-                    @forelse ($post->tags as $tag)
-                        {{ $tag->name }}{{ !$loop->last ? ',' : '' }}
-                    @empty
-                        -
-                    @endforelse
-                </p>
+
+    <section id="post_front_office">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-8 mx-auto">
+                    <div class="card-post">
+                        <div class="box-img">
+                            <img src="{{$post->img}}" alt="">
+                        </div>
+                        <div class="box-text">
+                            <div class="box-title">
+                                <a href="{{ route('posts.show', ['slug' => $post->slug]) }}"><h1>{{$post->title}}</h1>
+                                </a>
+                            </div>
+                            <div class="box-content">
+                                <p>{{$post->content}}</p>
+                            </div>
+                            <div class="box-category">
+                                <p>Categoria: {{ $post->category ? $post->category->name : '-' }}</p>
+                            </div>
+                            <div class="box-tag">
+                                <p>Tags:
+                                    @forelse ($post->tags as $tag)
+                                        {{ $tag->name }}{{ !$loop->last ? ',' : '' }}
+                                    @empty
+                                        -
+                                    @endforelse
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </section>
+
 @endsection
